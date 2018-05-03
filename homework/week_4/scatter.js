0bfaa8121b265109a70bfd0024cc865b8b857186
Najib <nelmibit@gmail.com>
2018-05-03 20:48:42 +0200

    saving progress

--- homework/week_4/scatter.js
+++ homework/week_4/scatter.js
@@ -1,10 +1,11 @@
+const planets = [];
+const velocities = [];
 
 
 window.onload = function() {
 
-  const planets = [];
-  const velocities = [];
-
+  const climates = [];
+  const starsXandY = [];
   const planetLink1 = "https://swapi.co/api/planets/";
   const planetLink2 = "https://swapi.co/api/planets/?page=2";
   const planetLink3 = "https://swapi.co/api/planets/?page=3";
@@ -55,18 +56,76 @@ window.onload = function() {
           && planets[i].rotation_period < 40) {
       C3PO.push(parseInt(planets[i].orbital_period));
       C3PO.push(parseInt(planets[i].rotation_period));
-      C3PO.push(parseInt(planets[i].diameter))
+      C3PO.push(parseInt(planets[i].diameter));
+      C3PO.push(planets[i].climate.split(', '));
       velocities.push(C3PO);
       }
     }
 
+    // color function
+    var categorical = { "name" : "schemeCategory20c", "n" : 20 };
+    var colorScale = d3.scaleOrdinal(d3[categorical.name])
+
     // svg element
+
+
+
+
     let svg = d3.select("body").append("svg")
+
+    svg.append("rect")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("fill", "black");
 
     // dimensions
     const width = 1000;
     const height = 500;
     const margin = {top: 20, right: 20, bottom: 30, left: 40};
+
+
+    //stars
+
+    for (let i = 0; i < 500; i++) {
+      let xy = [];
+      xy.push(20 + Math.random() * width);
+      xy.push(Math.random() * height);
+      starsXandY.push(xy);
+    }
+
+
+    // color color
+
+    function getColor(climate) {
+      if (climate.includes('temperate')) {
+          return 'green';
+      } else if (climate.includes('frozen')) {
+        return '#d8f0ff';
+      } else if (climate.includes('murky')) {
+        return '#636363';
+      } else if (climate.includes('arid')) {
+        return '#fdae6b';
+      } else if (climate.includes('windy')) {
+        return '#d9d9d9';
+      } else if (climate.includes('hot')) {
+        return '#e6550d';
+      } else if (climate.includes('tropical')) {
+        return '#31a354';
+      } else if (climate.includes('frigid')) {
+        return '#9ecae1';
+      } else if (climate.includes('humid')) {
+        return '#9ecae1';
+      } else if (climate.includes('polluted')) {
+        return '#9e9ac8';
+      } else if (climate.includes('superheated')) {
+        return '#e60d00';
+      } else if (climate.includes('artic') || climate.includes('subartic')) {
+        return '#3182bd';
+      }
+
+    }
+
+
 
 
     // scales
@@ -87,8 +146,12 @@ window.onload = function() {
           .attr("height", height + margin.top)
           .attr("class", "svg")
 
-          console.log(velocities)
-          console.log(planets)
+    for (let i = 0; i < velocities.length; i++) {
+      console.log(velocities[i][3]);
+    }
+
+
+
 
     svg.selectAll("circle")
         .data(velocities)
@@ -96,7 +159,24 @@ window.onload = function() {
         .append("circle")
         .attr('cx', d => xScale(d[0]) + margin.left)
         .attr('cy', d => yScale(d[1]) - margin.bottom)
-        .attr('r', 5)
+        .attr('r', d => (5 + (d[2] * 20) / 19720))
+        .attr("fill", d => getColor(d[3]))
+        .style('stroke', 'black')
+        .style('stroke-width', '1px')
+        .attr('class', 'planet')
+
+        svg.selectAll("ellipse")
+            .data(starsXandY)
+            .enter()
+            .append('circle')
+            .attr('cx', d => d[0])
+            .attr('cy', d => d[1])
+            .attr('r', 1)
+            .attr('fill', 'white')
+            .attr('class', 'stars')
+
+
+
 
     let xAxis = d3.axisBottom (xScale)
 
@@ -113,6 +193,8 @@ window.onload = function() {
         .attr('class', 'axis')
         .attr("transform", "translate(" + margin.right + ", -7)")
         .call(yAxis);
+
+
 
 
       }
