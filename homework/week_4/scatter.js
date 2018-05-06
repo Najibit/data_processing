1e20a164da68e6f513170043841e6f4a140b8f3c
Najib <nelmibit@gmail.com>
2018-05-06 12:36:40 +0200

    updating progress

--- homework/week_4/scatter.js
+++ homework/week_4/scatter.js
@@ -1,10 +1,10 @@
 const planets = [];
-const velocities = [];
+const spheres = [];
+const climates = [];
 
 
 window.onload = function() {
 
-  const climates = [];
   const starsXandY = [];
   const planetLink1 = "https://swapi.co/api/planets/";
   const planetLink2 = "https://swapi.co/api/planets/?page=2";
@@ -58,7 +58,8 @@ window.onload = function() {
       C3PO.push(parseInt(planets[i].rotation_period));
       C3PO.push(parseInt(planets[i].diameter));
       C3PO.push(planets[i].climate.split(', '));
-      velocities.push(C3PO);
+      C3PO.push(planets[i].name)
+      spheres.push(C3PO);
       }
     }
 
@@ -79,7 +80,7 @@ window.onload = function() {
         .attr("fill", "black");
 
     // dimensions
-    const width = 1000;
+    const width = 1200;
     const height = 500;
     const margin = {top: 20, right: 20, bottom: 30, left: 40};
 
@@ -88,7 +89,7 @@ window.onload = function() {
 
     for (let i = 0; i < 500; i++) {
       let xy = [];
-      xy.push(20 + Math.random() * width);
+      xy.push(20 + Math.random() * (width - 200));
       xy.push(Math.random() * height);
       starsXandY.push(xy);
     }
@@ -98,7 +99,7 @@ window.onload = function() {
 
     function getColor(climate) {
       if (climate.includes('temperate')) {
-          return '#00FF7F';
+          return '#00FFFF';
       } else if (climate.includes('frozen')) {
         return '#87CEFA';
       } else if (climate.includes('murky')) {
@@ -112,9 +113,9 @@ window.onload = function() {
       } else if (climate.includes('tropical')) {
         return '#31a354';
       } else if (climate.includes('frigid')) {
-        return '#9ecae1';
+        return '#FFF';
       } else if (climate.includes('humid')) {
-        return '#9ecae1';
+        return '#9ecaff';
       } else if (climate.includes('polluted')) {
         return '#6B8E23';
       } else if (climate.includes('superheated')) {
@@ -124,6 +125,8 @@ window.onload = function() {
       }
     }
 
+    climates.push('temperate', 'frozen', 'murky', 'arid', 'windy', 'hot', 'tropical', 'frigid', 'humid', 'polluted', 'superheated', 'artic');
+    climateColors = ['#00FFFF', '#87CEFA', '#D8BFD8', '#FFE4B5', '#2F4F4F', '#e6550d', '#31a354', '#FFF', '#9ecaff', '#6B8E23', '#e60d00', '#3182bd'];
     // var gradient = svg.append("svg:defs")
     //                   .append("svg:linearGradient")
     //                   .attr("id", "gradient")
@@ -149,42 +152,85 @@ window.onload = function() {
     // scales
     const xScale = d3.scaleLinear()
                         .domain([
-                          d3.min(velocities, d => d[0]),
-                          d3.max(velocities, d => d[0])
+                          100,
+                          d3.max(spheres, d => d[0] + 100)
                         ])
-                        .range([0, width]);
+                        .range([0, width - 200]);
     const yScale = d3.scaleLinear()
                         .domain([
-                          d3.min(velocities, d => d[1]),
-                          d3.max(velocities, d => d[1])
+                          d3.min(spheres, d => d[1] - 12),
+                          d3.max(spheres, d => d[1] + 10)
                         ])
                         .range([height, 0]);
 
-    svg.attr("width", width)
-          .attr("height", height + margin.top)
+    svg.attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
           .attr("class", "svg")
+          .attr("transform", "translate(0, 50)")
 
 
 
 
 
     svg.selectAll("circle")
-        .data(velocities)
+        .data(spheres)
         .enter()
         .append("circle")
         .attr('cx', d => xScale(d[0]) + margin.left)
-        .attr('cy', d => yScale(d[1]) - margin.bottom)
+        .attr('cy', d => yScale(d[1]))
         .attr('r', d => (5 + (d[2] * 20) / 19720))
         .attr("fill", d => getColor(d[3]))
         .style('stroke', 'black')
         .style('stroke-width', '1px')
         .attr('class', 'planet')
 
+        d3.select("svg").selectAll(".text")
+            .data(climates)
+            .enter().append("circle")
+            .attr("class", "text")
+            .attr("cx", width - 60)
+            .attr("cy", (d, i) => 10 + 30 * i)
+            .attr("r", 12)
+            .attr("fill", (d, i) => climateColors[i])
+            .attr("transform", "translate(-100, 75)")
+
+        d3.select("svg").selectAll(".climates")
+          		.data(climates)
+          		.enter().append("text")
+          		.attr("class", "climates")
+          		.attr("x", width - 130)
+          		.attr("y", (d, i) => 90 + 30 * i)
+          		.text((d, i) => climates[i])
+              .style('stroke', (d, i) => climateColors[i])
+
+        d3.select("svg")
+            .append("text")
+            .attr("class", "legendTitle")
+            .attr("x", width - 170)
+            .attr("y", 50)
+            .text("C L I M A T E S")
+            .style("stroke", "yellow")
+
+      d3.select("svg").append("text")
+            .attr("x", width / 2)
+            .attr("y", height)
+            .text("ORBIT AROUND PARENT STAR")
+            .attr("transform", "translate(-250, 50)")
+            .style("stroke", "yellow")
+            .style("font-size", "25px")
+
+      d3.select("svg").append("text")
+            .attr("x", -400)
+            .attr("y", -10)
+            // .attr("transform", "rotate(-90)")
+            .style("stroke", "yellow")
+            .style("font-size", "25px")
+            .text("ROTATION AROUND AXIS")
+            .attr("transform", "rotate(-90)")
+
 
 
 function starryNight (count) {
-  if (count < 0.5) {
-    console.log('1');
         svg.selectAll("ellipse")
             .data(starsXandY)
             .enter()
@@ -194,18 +240,6 @@ function starryNight (count) {
             .attr('r', 1)
             .attr('fill', 'yellow')
             .attr('class', 'stars')
-  } else {
-    console.log('2');
-    svg.selectAll("ellipse")
-        .data(starsXandY)
-        .enter()
-        .append('circle')
-        .attr('cx', d => d[0])
-        .attr('cy', d => d[1])
-        .attr('r', 1)
-        .attr('fill', 'black')
-        .attr('class', 'stars')
-  }
 }
 
 window.setInterval(starryNight(Math.random()), 1000);
@@ -218,13 +252,33 @@ window.setInterval(starryNight(Math.random()), 1000);
 
     svg.append("g")
         .attr('class', 'axis')
-        .attr("transform", "translate(10, " + height + ")")
+        .attr("transform", "translate(10, " + height   + ")")
         .call(xAxis);
 
 
     svg.append("g")
         .attr('class', 'axis')
-        .attr("transform", "translate(" + margin.right + ", -7)")
+        .attr("transform", "translate(" + margin.right + ", -3)")
         .call(yAxis);
-      }
+
+
+
+
+    // interactivity
+    response = spheres;
+    // implement interactivity by displaying count of crimes on hover-over
+    // create d3 tip
+  	var tip = d3.tip()
+  		.attr("class", "d3-tip")
+  		.offset([0, 0])
+
+  		// tip should display corresponding datavalues
+  		.html(function(d, i) {
+  			return "<p><strong>" + d[4] + "</strong></p><p><text>("
+  				+ d[0] + "," + d[1]
+  				+ ") </text></p>"
+  		})
+
+  	svg.call(tip)
+}
 }
