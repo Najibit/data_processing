69ada88d576fd9628c0393548a617e2f9cb1eddd
Najib <nelmibit@gmail.com>
2018-05-16 20:51:11 +0200

    saving progress

---
+++ homework/week_5/chart.js
@@ -0,0 +1,87 @@
+function makeChart(country) {
+
+    d3.select('#chart').remove();
+
+    const WIDTH = 500;
+    const HEIGHT = 250;
+    const MARGIN = {top: 50, bottom: 50, left: 60, right: 65};
+
+
+    var SVG = d3.select('body')
+                  .append('svg')
+                  .attr('width', WIDTH + MARGIN.left + MARGIN.right)
+                  .attr('height', HEIGHT + MARGIN.top + MARGIN.bottom)
+                  .style('background-color', '#efefef')
+                  .attr('class', 'svg')
+                  .attr('id', 'chart')
+
+
+    const xScale = d3.scaleLinear()
+                        .domain([0, artistCount])
+                        .range([MARGIN.left, WIDTH + MARGIN.right]);
+    const yScale = d3.scaleLinear()
+                        .domain([0, 6000000])
+                        .range([HEIGHT, 0]);
+
+
+    let barWidth = 45;
+
+
+    SVG.selectAll("rect")
+       .data(data[country])
+       .enter()
+       .append("rect")
+       .attr("x", (d, i) => MARGIN.left + 5 + i * (WIDTH / artistCount))
+       .attr("y", 0)
+       .attr("width", barWidth)
+       .attr("height", d => HEIGHT - yScale(d.listeners))
+       .style("opacity", 0)
+       .attr("fill", "gray")
+       .transition().duration(4000)
+       .delay((d, i) => i * 50)
+       .style("opacity", 1)
+       .attr("y", d => yScale(d.listeners))
+       .attr('fill', 'blue')
+       .attr('stroke', 'black')
+
+   SVG.selectAll("rect")
+       .data(data[country])
+       .on("mouseover", function() {
+         tooltip.style("display", null);
+       })
+       .on("mouseout", function() {
+         tooltip.style("display", "none");
+       })
+       .on("mousemove", function(d, i) {
+         let xPos =    d3.mouse(this)[0] - 15;
+         let yPos =    d3.mouse(this)[1] - 55;
+         tooltip.attr("transform", "translate(" + xPos +  "," + yPos + ")");
+         tooltip.select("text").text(d.name);
+       })
+
+     let tooltip = SVG.append("g")
+                       .attr("class", "tooltip")
+                       .style("display", "none")
+                       .style("font-weight", "bold")
+                       .style("font-size", "30px")
+                       .style("color", "red");
+
+     tooltip.append("text")
+             .attr("x", 15)
+             .attr("dy", "1.2em");
+
+
+
+     let xAxis = d3.axisBottom(xScale).ticks(0)
+     let yAxis = d3.axisLeft(yScale)
+
+     SVG.append("g")
+         .attr('class', 'axis')
+         .attr('transform', 'translate(0, ' + HEIGHT + ")")
+         .call(xAxis);
+
+     SVG.append("g")
+         .attr('class', 'axis')
+         .attr('transform', 'translate(' + MARGIN.left + ",0)")
+         .call(yAxis);
+}
--- homework/week_5/linked.js
+++ homework/week_5/linked.js
@@ -1,13 +1,21 @@
+
 /*
 Het is nog (lang) niet af. Het idee is om een kaart van de Europese Unie te hebben,
 waarbij je per land kan zien wat de top 10 muziekartiesten zijn. Ik kwam er echter halverwege achter,
 dat je enkel het aantal listens wereldwijd ziet, niet per land. Kan ik ook een scorebord maken per land met de top 10 artiesten?
-Of moet het een grafiek zijn?
+Of moet het een grafiek zijn
+
+Stappenplan:
+1. Van 1 land een staafdiagram maken
+2. Aan de hand van JQuery de staafdiagram laten veranderen
+3. Een landkaart instellen en aan de hand daarvan laten veranderen
+4. Met CSS styling doen
+5. Bootstrap functionaliteiten toevoegen.
 
 */
 
 
-let countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech+Republic', 'Denmark',
+let europeanUnion = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech+Republic', 'Denmark',
                   'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary',
                   'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta',
                   'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia',
@@ -22,15 +30,15 @@ let artistCount = 10;
 
 const API_LINKS = [];
 
-for (let i = 0; i < countries.length; i++) {
-  API_LINKS.push(URL + countries[i] + API_KEY + FORMAT);
+for (let i = 0; i < europeanUnion.length; i++) {
+  API_LINKS.push(URL + europeanUnion[i] + API_KEY + FORMAT);
 }
 
 window.onload = function() {
 
   const Q = d3.queue();
 
-  for (let i = 0; i < countries.length; i++) {
+  for (let i = 0; i < europeanUnion.length; i++) {
     Q.defer(d3.request, API_LINKS[i]);
   }
 
@@ -40,9 +48,6 @@ window.onload = function() {
   function shuffle(error, response) {
     if (error) throw error;
 
-    let information = JSON.parse(response[0].responseText);
-
-
     for (let i = 0; i < response.length; i++) {
       // make a dict for information per country
       let countryData = {};
@@ -50,14 +55,10 @@ window.onload = function() {
       // parse response to JSON
       let info = JSON.parse(response[i].responseText);
 
-      // max amount of artists per country
-      // let artistCount = 10;
-
       // all artist information
       let artists = [];
 
       // extract country names
-      // data['country'] = info.topartists['@attr'].country;
       let country = info.topartists['@attr'].country;
 
       // extract top 10 artists
@@ -71,68 +72,14 @@ window.onload = function() {
         artists.push(musician);
       }
 
-      // and add to info per country dict
-      countryData['artists'] = artists;
-      // push info per country dict to data array
       data[country] = artists;
-      // data.push(countryData);
     }
 
-    // start drawing
 
-    const WIDTH = 500;
-    const HEIGHT = 500;
-    const MARGIN = {top: 50, bottom: 50, left: 50, right: 50};
+let random = Math.floor(Math.random() * europeanUnion.length)
 
-    const SVG = d3.select('body')
-                    .append('svg')
-                    .attr('width', WIDTH + MARGIN.left + MARGIN.right)
-                    .attr('height', HEIGHT + MARGIN.top + MARGIN.bottom)
-                    .style('background-color', '#efefef')
-                    .attr('class', 'svg')
+      createMap();
 
-
-
-    const xScale = d3.scaleLinear()
-                        .domain([0, artistCount])
-                        .range([MARGIN.left, WIDTH + MARGIN.right]);
-    const yScale = d3.scaleLinear()
-                        .domain([0, 6000000])
-                        .range([HEIGHT, 0]);
-
-    console.log(yScale(data.Austria[0].listeners))
-    console.log(data.Austria[0].listeners);
-
-    SVG.selectAll("rect")
-       .data(data.Austria)
-       .enter()
-       .append("rect")
-       .attr("class", "bars")
-       .attr("x", 50)
-       .attr("y", (d, i) => i * 40)
-       .attr("width", 500)
-       .attr("height", 35)
-       .attr('fill', 'white')
-       .attr('stroke', 'black')
-       .text(data.Austria.listeners)
-
-
-       // let xAxis = d3.axisBottom(xScale).ticks(0)
-       // let yAxis = d3.axisLeft(yScale)
-
-     //   SVG.append("g")
-     //       .attr('class', 'axis')
-     //       .attr('transform', 'translate(0, ' + HEIGHT + ")")
-     //       .call(xAxis);
-     //
-     //   SVG.append("g")
-     //       .attr('class', 'axis')
-     //       .attr('transform', 'translate(' + MARGIN.left + ",0)")
-     //       .call(yAxis);
-     //
-     // }
-
-}
-
+     }
 
 }
