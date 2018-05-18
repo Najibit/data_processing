585b426716d1016866d1637df26dacf6f645ee2a
Najib <nelmibit@gmail.com>
2018-05-18 23:58:44 +0200

    saving progress 3

--- homework/week_5/scripts/linked.js
+++ homework/week_5/scripts/linked.js
@@ -22,7 +22,7 @@ let europeanUnion = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Cze
                   'Slovenia', 'Spain', 'Sweden', 'United+Kingdom'];
 
 const API_KEY = "&api_key=3720eb4ef07788c0a16540ceda7dcb61";
-const URL = "http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=";
+const URL = "https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=";
 const FORMAT = "&format=json&limit=10";
 const data = {};
 let artistCount = 10;
